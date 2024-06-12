0793ad548021d6f1ef2668f474ab68c4a89f461d
Shahrokh1383 <shahrokh.hatami2004@gmail.com>
2024-06-13 03:02:11 +0330

    Add files via upload

---
+++ practice.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	int n;
+	char c;
+	cout << "enter the number : ";
+	cin >> n;
+	cout << "enter the character : ";
+	cin >> c;
+	for (int i = 0; i < n; i++)
+	{
+		cout << c;
+	}
+	return 0;
+}
